49c682dff88aa50ea913d7d0d70774bd3f443fda
dfrid <dfrid@stuy.edu>
2020-02-03 17:05:23 -0500

    removed mess

--- machinaLearn.py
+++ machinaLearn.py
@@ -3,77 +3,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # Install TensorFlow
 
 from tensorflow.keras import datasets, layers, models
-import os
-from thresh import *
 from trainer import Trainer
-from numpy import random
 
 
-def makeImgArr(path, label):
-    img_size = 30
-    folder, dirs, files = next(os.walk(path))
-    file_count = len(files)
-    train_len = int(file_count * .8)
-    test_len = int(file_count * .2)
-    train_images = np.ndarray(shape=(train_len, img_size, img_size))
-    train_labels = np.ndarray(shape=(train_len, 1))
-    test_images = np.ndarray(shape=(test_len, img_size, img_size))
-    test_labels = np.ndarray(shape=(test_len, 1))
-    for i, file in enumerate(os.listdir(path)):
-        if (i < train_len):
-            train_images[i] = cv2.imread(path + file, 0)
-            train_labels[i] = label
-        else:
-            test_images[i - train_len] = cv2.imread(path + file, 0)
-            test_labels[i - train_len] = label
-
-    return train_images,train_labels,test_images,test_labels
-
-
-
-#print(img)
-'''path = "test_1/"
-train_images = np.ndarray(shape=(2400,30,30))
-train_labels = np.ndarray(shape=(2400,1))
-test_images = np.ndarray(shape=(600,30,30))
-test_labels = np.ndarray(shape=(600,1))
-for i,file in enumerate(os.listdir(path)):
-    if (i < 1200):
-        train_images[i] = cv2.imread(path + file, cv2.IMREAD_UNCHANGED)
-        train_labels[i] = 0
-    else:
-        test_images[i-1200]= cv2.imread(path + file, cv2.IMREAD_UNCHANGED)
-        test_labels[i-1200]= 0
-
-
-
-
-path = 'test_2/'
-for i,file in enumerate(os.listdir(path)):
-    if (i < 1200):
-        train_images[i+1200] = cv2.imread(path + file, cv2.IMREAD_UNCHANGED)
-        train_labels[i+1200] = 1
-    else:
-        test_images[i-900]= cv2.imread(path + file, cv2.IMREAD_UNCHANGED)
-        test_labels[i-900]= 1
-
-def shuffleImgs(imgs,labels):
-    rng_state = random.get_state()
-    random.shuffle(imgs)
-    random.set_state(rng_state)
-    random.shuffle(labels)'''
-
-
-
-'''shuffleImgs(train_images,train_labels)
-train_images, test_images = train_images / 255.0, test_images / 255.0
-train_images = train_images.reshape([2400,30,30,1])
-test_images = test_images.reshape([600,30,30,1])'''
-
-'''cv2.namedWindow('yer')
-for img in train_images:
-    cv2.imshow('yer',img)
-    cv2.waitKey(50)'''
 
 path1 = 'test_1/'
 path2 = 'test_2/'
--- thresh.py
+++ thresh.py
@@ -3,65 +3,7 @@ import numpy as np
 
 
 
-def getGrayHand(img):
-    lower = np.array([0, 133, 77], np.uint8)
-    upper = np.array([255, 173, 127], np.uint8)
-    imageYcr = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
-    skinRegion = cv2.inRange(imageYcr, lower, upper)
-    res = cv2.bitwise_and(imageYcr, imageYcr, mask=skinRegion)
-    cv2.cvtColor(res, cv2.COLOR_YCrCb2BGR)
-    gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    ret, final = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    return final
-# print(skinRegion)
-
-
-#function to remove extra blackspace
-def reSpace(img):
-    minx = 1000
-    miny = 1000
-    maxx = 0
-    maxy= 0
-    for y,row in enumerate(img):
-        for x,grayValue in enumerate(row):
-            if(grayValue > 10):
-                if(x < minx):
-                    minx = x
-                elif(x > maxx):
-                    maxx = x
-                if(y < miny):
-                    miny = y
-                elif(y > maxy):
-                    maxy = y
-    return img[miny:maxy, minx:maxx]
-
-def getfinalImg(img):
-    ret = getGrayHand(img)
-    ret = reSpace(ret)
-    return ret
-
-def drawContourBin(img):
-    ret, thresh = cv2.threshold(img, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if (len(contours) > 0):
-        contours = max(contours, key= lambda x:cv2.contourArea(x))
-        print(contours)
-    return img
-
-
-def drawSkin(img):
-    lower = np.array([0, 133, 77], np.uint8)
-    upper = np.array([255, 173, 127], np.uint8)
-    imageYcr = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
-    skinRegion = cv2.inRange(imageYcr, lower, upper)
-    contours, hierarchy = cv2.findContours(skinRegion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for i, c in enumerate(contours):
-        area = cv2.contourArea(c)
-        if area > 1000:
-            cv2.drawContours(img, contours, i, (0, 255, 0), 3)
-    return img
-
-def preprocess(action_frame):
+ def preprocess(action_frame):
 
     blur = cv2.GaussianBlur(action_frame, (3,3), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_RGB2HSV)
